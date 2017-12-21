5dc28d8619121401c689a783d594de58155c99f2
Abdelrahman Omran <me@omranic.com>
2017-12-21 21:16:22 +0200

    Tweak menus

--- routes/menus.php
+++ routes/menus.php
@@ -7,14 +7,14 @@ use Rinvex\Menus\Factories\MenuFactory;
 
 Menu::modify('adminarea.sidebar', function(MenuFactory $menu) {
     $menu->dropdown(function (MenuItem $dropdown) {
-        $dropdown->route(['adminarea.rooms.index'], trans('cortex/bookings::common.rooms'), 10, 'fa fa-cubes')->can('list-rooms');
-        $dropdown->route(['adminarea.bookings.index'], trans('cortex/bookings::common.bookings'), 20, 'fa fa-calendar')->can('list-bookings');
+        $dropdown->route(['adminarea.rooms.index'], trans('cortex/bookings::common.rooms'), 10, 'fa fa-cubes')->ifCan('list-rooms')->activateOnRoute('adminarea.rooms');
+        $dropdown->route(['adminarea.bookings.index'], trans('cortex/bookings::common.bookings'), 20, 'fa fa-calendar')->ifCan('list-bookings')->activateOnRoute('adminarea.bookings');
     }, trans('cortex/bookings::common.space'), 60, 'fa fa-code-fork');
 });
 
 Menu::modify('managerarea.sidebar', function(MenuFactory $menu) {
     $menu->dropdown(function (MenuItem $dropdown) {
-        $dropdown->route(['managerarea.rooms.index'], trans('cortex/bookings::common.rooms'), 10, 'fa fa-cubes')->can('list-rooms');
-        $dropdown->route(['managerarea.bookings.index'], trans('cortex/bookings::common.bookings'), 20, 'fa fa-calendar')->can('list-bookings');
+        $dropdown->route(['managerarea.rooms.index'], trans('cortex/bookings::common.rooms'), 10, 'fa fa-cubes')->ifCan('list-rooms')->activateOnRoute('managerarea.rooms');
+        $dropdown->route(['managerarea.bookings.index'], trans('cortex/bookings::common.bookings'), 20, 'fa fa-calendar')->ifCan('list-bookings')->activateOnRoute('managerarea.bookings');
     }, trans('cortex/bookings::common.space'), 60, 'fa fa-code-fork');
 });
