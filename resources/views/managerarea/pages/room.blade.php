a258237d4f935c541d100aa7a9dc7c428b2bd684
Abdelrahman Omran <me@omranic.com>
2018-01-15 19:57:58 +0200

    Prevent turbolinks caching pages with datatables

--- resources/views/managerarea/pages/room.blade.php
+++ resources/views/managerarea/pages/room.blade.php
@@ -227,6 +227,10 @@
 
 @if($room->exists)
 
+    @push('head-elements')
+        <meta name="turbolinks-cache-control" content="no-cache">
+    @endpush
+
     @push('styles')
         <link href="{{ mix('css/datatables.css', 'assets') }}" rel="stylesheet">
     @endpush
