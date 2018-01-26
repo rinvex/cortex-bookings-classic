bdafe249ffd099129e36e03e9537f2b21b91d2a1
Abdelrahman Omran <me@omranic.com>
2018-01-26 15:41:19 +0200

    Drop useless transformers in favor to model attribute casting

--- src/DataTables/Adminarea/RoomsDataTable.php
+++ src/DataTables/Adminarea/RoomsDataTable.php
@@ -6,7 +6,6 @@ namespace Cortex\Bookings\DataTables\Adminarea;
 
 use Cortex\Bookings\Models\Room;
 use Cortex\Foundation\DataTables\AbstractDataTable;
-use Cortex\Bookings\Transformers\Adminarea\RoomTransformer;
 
 class RoomsDataTable extends AbstractDataTable
 {
@@ -14,11 +13,6 @@ class RoomsDataTable extends AbstractDataTable
      * {@inheritdoc}
      */
     protected $model = Room::class;
-
-    /**
-     * {@inheritdoc}
-     */
-    protected $transformer = RoomTransformer::class;
 
     /**
      * Get the query object to be processed by dataTables.
@@ -40,10 +34,7 @@ class RoomsDataTable extends AbstractDataTable
      */
     public function ajax()
     {
-        $transformer = app($this->transformer);
-
         return datatables($this->query())
-            ->setTransformer($transformer)
             ->orderColumn('name', 'name->"$.'.app()->getLocale().'" $1')
             ->make(true);
     }
@@ -62,7 +53,7 @@ class RoomsDataTable extends AbstractDataTable
         return [
             'name' => ['title' => trans('cortex/bookings::common.name'), 'render' => $link.'+(full.is_active ? " <i class=\"text-success fa fa-check\"></i>" : " <i class=\"text-danger fa fa-close\"></i>")', 'responsivePriority' => 0],
             'price' => ['title' => trans('cortex/bookings::common.price')],
-            'unit' => ['title' => trans('cortex/bookings::common.unit')],
+            'unit' => ['title' => trans('cortex/bookings::common.unit'), 'render' => 'Lang.trans(\'cortex/bookings::common.unit_\'+data)'],
             'currency' => ['title' => trans('cortex/bookings::common.currency')],
             'sort_order' => ['title' => trans('cortex/bookings::common.sort_order'), 'visible' => false],
             'created_at' => ['title' => trans('cortex/bookings::common.created_at'), 'render' => "moment(data).format('MMM Do, YYYY')"],
