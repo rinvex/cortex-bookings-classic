940d725fca7c69ad31c96277f20221a94364cc13
Abdelrahman Omran <me@omranic.com>
2018-02-15 17:05:13 +0200

    Add auditable fields to bookings

---
+++ database/migrations/2018_01_31_125745_alter_bookings_table_add_auditable_columns.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterBookingsTableAddAuditableColumns extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table(config('rinvex.bookings.tables.bookings'), function (Blueprint $table) {
+            $table->auditable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table(config('rinvex.bookings.tables.bookings'), function (Blueprint $table) {
+            $table->dropAuditable();
+        });
+    }
+}
