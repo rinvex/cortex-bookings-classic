33f3a0fb2f3820b42cd6c650a6ab23607ab5f87e
Abdelrahman Omran <me@omranic.com>
2018-03-28 20:45:09 +0200

    Add missing abilities

--- database/seeds/CortexBookingsSeeder.php
+++ database/seeds/CortexBookingsSeeder.php
@@ -14,12 +14,14 @@ class CortexBookingsSeeder extends Seeder
     public function run()
     {
         Bouncer::allow('admin')->to('list', config('cortex.bookings.models.room'));
+        Bouncer::allow('admin')->to('import', config('cortex.bookings.models.room'));
         Bouncer::allow('admin')->to('create', config('cortex.bookings.models.room'));
         Bouncer::allow('admin')->to('update', config('cortex.bookings.models.room'));
         Bouncer::allow('admin')->to('delete', config('cortex.bookings.models.room'));
         Bouncer::allow('admin')->to('audit', config('cortex.bookings.models.room'));
 
         Bouncer::allow('admin')->to('list', config('cortex.bookings.models.event'));
+        Bouncer::allow('admin')->to('import', config('cortex.bookings.models.event'));
         Bouncer::allow('admin')->to('create', config('cortex.bookings.models.event'));
         Bouncer::allow('admin')->to('update', config('cortex.bookings.models.event'));
         Bouncer::allow('admin')->to('delete', config('cortex.bookings.models.event'));
@@ -45,5 +47,40 @@ class CortexBookingsSeeder extends Seeder
         Bouncer::allow('admin')->to('create', config('rinvex.bookings.models.availability'));
         Bouncer::allow('admin')->to('update', config('rinvex.bookings.models.availability'));
         Bouncer::allow('admin')->to('delete', config('rinvex.bookings.models.availability'));
+
+        Bouncer::allow('owner')->to('list', config('cortex.bookings.models.room'));
+        Bouncer::allow('owner')->to('import', config('cortex.bookings.models.room'));
+        Bouncer::allow('owner')->to('create', config('cortex.bookings.models.room'));
+        Bouncer::allow('owner')->to('update', config('cortex.bookings.models.room'));
+        Bouncer::allow('owner')->to('delete', config('cortex.bookings.models.room'));
+        Bouncer::allow('owner')->to('audit', config('cortex.bookings.models.room'));
+
+        Bouncer::allow('owner')->to('list', config('cortex.bookings.models.event'));
+        Bouncer::allow('owner')->to('import', config('cortex.bookings.models.event'));
+        Bouncer::allow('owner')->to('create', config('cortex.bookings.models.event'));
+        Bouncer::allow('owner')->to('update', config('cortex.bookings.models.event'));
+        Bouncer::allow('owner')->to('delete', config('cortex.bookings.models.event'));
+        Bouncer::allow('owner')->to('audit', config('cortex.bookings.models.event'));
+
+        Bouncer::allow('owner')->to('list', config('rinvex.bookings.models.booking'));
+        Bouncer::allow('owner')->to('create', config('rinvex.bookings.models.booking'));
+        Bouncer::allow('owner')->to('update', config('rinvex.bookings.models.booking'));
+        Bouncer::allow('owner')->to('delete', config('rinvex.bookings.models.booking'));
+        Bouncer::allow('owner')->to('audit', config('rinvex.bookings.models.booking'));
+
+        Bouncer::allow('owner')->to('list', config('rinvex.bookings.models.addon'));
+        Bouncer::allow('owner')->to('create', config('rinvex.bookings.models.addon'));
+        Bouncer::allow('owner')->to('update', config('rinvex.bookings.models.addon'));
+        Bouncer::allow('owner')->to('delete', config('rinvex.bookings.models.addon'));
+
+        Bouncer::allow('owner')->to('list', config('rinvex.bookings.models.rate'));
+        Bouncer::allow('owner')->to('create', config('rinvex.bookings.models.rate'));
+        Bouncer::allow('owner')->to('update', config('rinvex.bookings.models.rate'));
+        Bouncer::allow('owner')->to('delete', config('rinvex.bookings.models.rate'));
+
+        Bouncer::allow('owner')->to('list', config('rinvex.bookings.models.availability'));
+        Bouncer::allow('owner')->to('create', config('rinvex.bookings.models.availability'));
+        Bouncer::allow('owner')->to('update', config('rinvex.bookings.models.availability'));
+        Bouncer::allow('owner')->to('delete', config('rinvex.bookings.models.availability'));
     }
 }
