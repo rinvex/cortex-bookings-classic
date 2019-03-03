33233299231aeda06967acc36a9e9e05a3050dfb
Abdelrahman Omran <me@omranic.com>
2019-03-03 17:42:07 +0400

    Refactor abilities seeding

--- database/seeds/CortexBookingsSeeder.php
+++ database/seeds/CortexBookingsSeeder.php
@@ -13,72 +13,45 @@ class CortexBookingsSeeder extends Seeder
      */
     public function run()
     {
-        Bouncer::allow('admin')->to('list', config('cortex.bookings.models.service'));
-        Bouncer::allow('admin')->to('import', config('cortex.bookings.models.service'));
-        Bouncer::allow('admin')->to('create', config('cortex.bookings.models.service'));
-        Bouncer::allow('admin')->to('update', config('cortex.bookings.models.service'));
-        Bouncer::allow('admin')->to('delete', config('cortex.bookings.models.service'));
-        Bouncer::allow('admin')->to('audit', config('cortex.bookings.models.service'));
+        $abilities = [
+            ['name' => 'list', 'title' => 'List services', 'entity_type' => 'service'],
+            ['name' => 'import', 'title' => 'Import services', 'entity_type' => 'service'],
+            ['name' => 'create', 'title' => 'Create services', 'entity_type' => 'service'],
+            ['name' => 'update', 'title' => 'Update services', 'entity_type' => 'service'],
+            ['name' => 'delete', 'title' => 'Delete services', 'entity_type' => 'service'],
+            ['name' => 'audit', 'title' => 'Audit services', 'entity_type' => 'service'],
 
-        Bouncer::allow('admin')->to('list', config('cortex.bookings.models.event'));
-        Bouncer::allow('admin')->to('import', config('cortex.bookings.models.event'));
-        Bouncer::allow('admin')->to('create', config('cortex.bookings.models.event'));
-        Bouncer::allow('admin')->to('update', config('cortex.bookings.models.event'));
-        Bouncer::allow('admin')->to('delete', config('cortex.bookings.models.event'));
-        Bouncer::allow('admin')->to('audit', config('cortex.bookings.models.event'));
+            ['name' => 'list', 'title' => 'List event', 'entity_type' => 'event'],
+            ['name' => 'import', 'title' => 'Import event', 'entity_type' => 'event'],
+            ['name' => 'create', 'title' => 'Create event', 'entity_type' => 'event'],
+            ['name' => 'update', 'title' => 'Update event', 'entity_type' => 'event'],
+            ['name' => 'delete', 'title' => 'Delete event', 'entity_type' => 'event'],
+            ['name' => 'audit', 'title' => 'Audit event', 'entity_type' => 'event'],
 
-        Bouncer::allow('admin')->to('list', config('cortex.bookings.models.event_ticket'));
-        Bouncer::allow('admin')->to('create', config('cortex.bookings.models.event_ticket'));
-        Bouncer::allow('admin')->to('update', config('cortex.bookings.models.event_ticket'));
-        Bouncer::allow('admin')->to('delete', config('cortex.bookings.models.event_ticket'));
-        Bouncer::allow('admin')->to('audit', config('cortex.bookings.models.event_ticket'));
+            ['name' => 'list', 'title' => 'List event tickets', 'entity_type' => 'event_ticket'],
+            ['name' => 'import', 'title' => 'Import event tickets', 'entity_type' => 'event_ticket'],
+            ['name' => 'create', 'title' => 'Create event tickets', 'entity_type' => 'event_ticket'],
+            ['name' => 'update', 'title' => 'Update event tickets', 'entity_type' => 'event_ticket'],
+            ['name' => 'delete', 'title' => 'Delete event tickets', 'entity_type' => 'event_ticket'],
+            ['name' => 'audit', 'title' => 'Audit event tickets', 'entity_type' => 'event_ticket'],
 
-        Bouncer::allow('admin')->to('list', config('cortex.bookings.models.service_rate'));
-        Bouncer::allow('admin')->to('import', config('cortex.bookings.models.service_rate'));
-        Bouncer::allow('admin')->to('create', config('cortex.bookings.models.service_rate'));
-        Bouncer::allow('admin')->to('update', config('cortex.bookings.models.service_rate'));
-        Bouncer::allow('admin')->to('delete', config('cortex.bookings.models.service_rate'));
-        Bouncer::allow('admin')->to('audit', config('cortex.bookings.models.service_rate'));
+            ['name' => 'list', 'title' => 'List service rates', 'entity_type' => 'service_rate'],
+            ['name' => 'import', 'title' => 'Import service rates', 'entity_type' => 'service_rate'],
+            ['name' => 'create', 'title' => 'Create service rates', 'entity_type' => 'service_rate'],
+            ['name' => 'update', 'title' => 'Update service rates', 'entity_type' => 'service_rate'],
+            ['name' => 'delete', 'title' => 'Delete service rates', 'entity_type' => 'service_rate'],
+            ['name' => 'audit', 'title' => 'Audit service rates', 'entity_type' => 'service_rate'],
 
-        Bouncer::allow('admin')->to('list', config('cortex.bookings.models.service_availability'));
-        Bouncer::allow('admin')->to('import', config('cortex.bookings.models.service_availability'));
-        Bouncer::allow('admin')->to('create', config('cortex.bookings.models.service_availability'));
-        Bouncer::allow('admin')->to('update', config('cortex.bookings.models.service_availability'));
-        Bouncer::allow('admin')->to('delete', config('cortex.bookings.models.service_availability'));
-        Bouncer::allow('admin')->to('audit', config('cortex.bookings.models.service_availability'));
+            ['name' => 'list', 'title' => 'List service availabilities', 'entity_type' => 'service_availability'],
+            ['name' => 'import', 'title' => 'Import service availabilities', 'entity_type' => 'service_availability'],
+            ['name' => 'create', 'title' => 'Create service availabilities', 'entity_type' => 'service_availability'],
+            ['name' => 'update', 'title' => 'Update service availabilities', 'entity_type' => 'service_availability'],
+            ['name' => 'delete', 'title' => 'Delete service availabilities', 'entity_type' => 'service_availability'],
+            ['name' => 'audit', 'title' => 'Audit service availabilities', 'entity_type' => 'service_availability'],
+        ];
 
-        Bouncer::allow('owner')->to('list', config('cortex.bookings.models.service'));
-        Bouncer::allow('owner')->to('import', config('cortex.bookings.models.service'));
-        Bouncer::allow('owner')->to('create', config('cortex.bookings.models.service'));
-        Bouncer::allow('owner')->to('update', config('cortex.bookings.models.service'));
-        Bouncer::allow('owner')->to('delete', config('cortex.bookings.models.service'));
-        Bouncer::allow('owner')->to('audit', config('cortex.bookings.models.service'));
-
-        Bouncer::allow('owner')->to('list', config('cortex.bookings.models.event'));
-        Bouncer::allow('owner')->to('import', config('cortex.bookings.models.event'));
-        Bouncer::allow('owner')->to('create', config('cortex.bookings.models.event'));
-        Bouncer::allow('owner')->to('update', config('cortex.bookings.models.event'));
-        Bouncer::allow('owner')->to('delete', config('cortex.bookings.models.event'));
-        Bouncer::allow('owner')->to('audit', config('cortex.bookings.models.event'));
-
-        Bouncer::allow('owner')->to('list', config('cortex.bookings.models.event_ticket'));
-        Bouncer::allow('owner')->to('create', config('cortex.bookings.models.event_ticket'));
-        Bouncer::allow('owner')->to('update', config('cortex.bookings.models.event_ticket'));
-        Bouncer::allow('owner')->to('delete', config('cortex.bookings.models.event_ticket'));
-        Bouncer::allow('owner')->to('audit', config('cortex.bookings.models.event_ticket'));
-
-        Bouncer::allow('owner')->to('list', config('cortex.bookings.models.service_rate'));
-        Bouncer::allow('owner')->to('import', config('cortex.bookings.models.service_rate'));
-        Bouncer::allow('owner')->to('create', config('cortex.bookings.models.service_rate'));
-        Bouncer::allow('owner')->to('update', config('cortex.bookings.models.service_rate'));
-        Bouncer::allow('owner')->to('delete', config('cortex.bookings.models.service_rate'));
-        Bouncer::allow('owner')->to('audit', config('cortex.bookings.models.service_rate'));
-
-        Bouncer::allow('owner')->to('list', config('cortex.bookings.models.service_availability'));
-        Bouncer::allow('owner')->to('import', config('cortex.bookings.models.service_availability'));
-        Bouncer::allow('owner')->to('create', config('cortex.bookings.models.service_availability'));
-        Bouncer::allow('owner')->to('update', config('cortex.bookings.models.service_availability'));
-        Bouncer::allow('owner')->to('delete', config('cortex.bookings.models.service_availability'));
-        Bouncer::allow('owner')->to('audit', config('cortex.bookings.models.service_availability'));
+        collect($abilities)->each(function (array $ability) {
+            app('cortex.auth.ability')->create($ability);
+        });
     }
 }
