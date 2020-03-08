aaca54525d07e02ec2df3d050a675aabc2a5f539
Abdelrahman Omran <me@omranic.com>
2020-03-09 01:51:33 +0400

    Add webpack module requirements

---
+++ resources/js/webpack.js
@@ -0,0 +1,17 @@
+module.exports = {
+    scanForCssSelectors: [
+        path.join(__dirname, 'node_modules/fullcalendar/**/*.js'),
+        path.join(__dirname, 'node_modules/fullcalendar-scheduler/**/*.js'),
+    ],
+    whitelistPatterns: [],
+    webpackPlugins: [],
+    install: ['fullcalendar'],
+    mix: {
+        css: [
+            {input: 'resources/sass/fullcalendar.scss', output: 'public/css/fullcalendar.css'},
+        ],
+        js: [
+            {input: 'resources/js/vendor/fullcalendar.js', output: 'public/js/fullcalendar.js'},
+        ],
+    },
+};
