e4bddc6771bf13ef48b9ee256655ad913d17d25e
Abdelrahman Omran <me@omranic.com>
2020-03-30 05:55:08 +0400

    Enforce consistent artisan command tag namespacing

--- src/Console/Commands/PublishCommand.php
+++ src/Console/Commands/PublishCommand.php
@@ -33,22 +33,22 @@ class PublishCommand extends BasePublishCommand
 
         switch ($this->option('resource')) {
             case 'lang':
-                $this->call('vendor:publish', ['--tag' => 'cortex-bookings-lang', '--force' => $this->option('force')]);
+                $this->call('vendor:publish', ['--tag' => 'cortex/bookings::lang', '--force' => $this->option('force')]);
                 break;
             case 'views':
-                $this->call('vendor:publish', ['--tag' => 'cortex-bookings-views', '--force' => $this->option('force')]);
+                $this->call('vendor:publish', ['--tag' => 'cortex/bookings::views', '--force' => $this->option('force')]);
                 break;
             case 'config':
-                $this->call('vendor:publish', ['--tag' => 'cortex-bookings-config', '--force' => $this->option('force')]);
+                $this->call('vendor:publish', ['--tag' => 'cortex/bookings::config', '--force' => $this->option('force')]);
                 break;
             case 'migrations':
-                $this->call('vendor:publish', ['--tag' => 'cortex-bookings-migrations', '--force' => $this->option('force')]);
+                $this->call('vendor:publish', ['--tag' => 'cortex/bookings::migrations', '--force' => $this->option('force')]);
                 break;
             default:
-                $this->call('vendor:publish', ['--tag' => 'cortex-bookings-lang', '--force' => $this->option('force')]);
-                $this->call('vendor:publish', ['--tag' => 'cortex-bookings-views', '--force' => $this->option('force')]);
-                $this->call('vendor:publish', ['--tag' => 'cortex-bookings-config', '--force' => $this->option('force')]);
-                $this->call('vendor:publish', ['--tag' => 'cortex-bookings-migrations', '--force' => $this->option('force')]);
+                $this->call('vendor:publish', ['--tag' => 'cortex/bookings::lang', '--force' => $this->option('force')]);
+                $this->call('vendor:publish', ['--tag' => 'cortex/bookings::views', '--force' => $this->option('force')]);
+                $this->call('vendor:publish', ['--tag' => 'cortex/bookings::config', '--force' => $this->option('force')]);
+                $this->call('vendor:publish', ['--tag' => 'cortex/bookings::migrations', '--force' => $this->option('force')]);
                 break;
         }
 
