71cdb5ef6a5ae3b9cf32faa403541600301a3947
Abdelrahman Omran <me@omranic.com>
2020-08-25 02:13:01 +0400

    Enforce controller API consistency

--- routes/web/adminarea.php
+++ routes/web/adminarea.php
@@ -15,10 +15,10 @@ Route::domain(domain())->group(function () {
                  Route::post('import')->name('stash')->uses('ServicesController@stash');
                  Route::post('hoard')->name('hoard')->uses('ServicesController@hoard');
                  Route::get('import/logs')->name('import.logs')->uses('ServicesController@importLogs');
-                 Route::get('create')->name('create')->uses('ServicesController@form');
+                 Route::get('create')->name('create')->uses('ServicesController@create');
                  Route::post('create')->name('store')->uses('ServicesController@store');
                  Route::get('{service}')->name('show')->uses('ServicesController@show');
-                 Route::get('{service}/edit')->name('edit')->uses('ServicesController@form');
+                 Route::get('{service}/edit')->name('edit')->uses('ServicesController@edit');
                  Route::put('{service}/edit')->name('update')->uses('ServicesController@update');
                  Route::get('{service}/logs')->name('logs')->uses('ServicesController@logs');
                  Route::delete('{service}')->name('destroy')->uses('ServicesController@destroy');
@@ -36,9 +36,9 @@ Route::domain(domain())->group(function () {
                      Route::post('import')->name('stash')->uses('ServiceBookingsController@stash');
                      Route::post('hoard')->name('hoard')->uses('ServiceBookingsController@hoard');
                      Route::get('import/logs')->name('import.logs')->uses('ServiceBookingsController@importLogs');
-                     Route::get('create')->name('create')->uses('ServiceBookingsController@form');
+                     Route::get('create')->name('create')->uses('ServiceBookingsController@create');
                      Route::post('create')->name('store')->uses('ServiceBookingsController@store');
-                     Route::get('{service_booking}')->name('edit')->uses('ServiceBookingsController@form');
+                     Route::get('{service_booking}')->name('edit')->uses('ServiceBookingsController@edit');
                      Route::put('{service_booking}')->name('update')->uses('ServiceBookingsController@update');
                      Route::get('{service_booking}/logs')->name('logs')->uses('ServiceBookingsController@logs');
                      Route::delete('{service_booking}')->name('destroy')->uses('ServiceBookingsController@destroy');
@@ -52,10 +52,10 @@ Route::domain(domain())->group(function () {
                  Route::post('import')->name('stash')->uses('EventsController@stash');
                  Route::post('hoard')->name('hoard')->uses('EventsController@hoard');
                  Route::get('import/logs')->name('import.logs')->uses('EventsController@importLogs');
-                 Route::get('create')->name('create')->uses('EventsController@form');
+                 Route::get('create')->name('create')->uses('EventsController@create');
                  Route::post('create')->name('store')->uses('EventsController@store');
                  Route::get('{event}')->name('show')->uses('EventsController@show');
-                 Route::get('{event}/edit')->name('edit')->uses('EventsController@form');
+                 Route::get('{event}/edit')->name('edit')->uses('EventsController@edit');
                  Route::put('{event}/edit')->name('update')->uses('EventsController@update');
                  Route::get('{event}/logs')->name('logs')->uses('EventsController@logs');
                  Route::delete('{event}')->name('destroy')->uses('EventsController@destroy');
@@ -69,10 +69,10 @@ Route::domain(domain())->group(function () {
                  // Tickets Routes
                  Route::name('tickets.')->prefix('{event}/tickets')->group(function () {
                      Route::match(['get', 'post'], '/')->name('index')->uses('EventTicketsController@index');
-                     Route::get('create')->name('create')->uses('EventTicketsController@form');
+                     Route::get('create')->name('create')->uses('EventTicketsController@create');
                      Route::post('create')->name('store')->uses('EventTicketsController@store');
                      Route::get('{event_ticket}')->name('show')->uses('EventTicketsController@show');
-                     Route::get('{event_ticket}/edit')->name('edit')->uses('EventTicketsController@form');
+                     Route::get('{event_ticket}/edit')->name('edit')->uses('EventTicketsController@edit');
                      Route::put('{event_ticket}/edit')->name('update')->uses('EventTicketsController@update');
                      Route::delete('{event_ticket}')->name('destroy')->uses('EventTicketsController@destroy');
                  });
@@ -83,10 +83,10 @@ Route::domain(domain())->group(function () {
                      Route::get('import')->name('import')->uses('EventBookingsController@import');
                      Route::post('import')->name('stash')->uses('EventBookingsController@stash');
                      Route::post('hoard')->name('hoard')->uses('EventBookingsController@hoard');
-                     Route::get('create')->name('create')->uses('EventBookingsController@form');
+                     Route::get('create')->name('create')->uses('EventBookingsController@create');
                      Route::post('create')->name('store')->uses('EventBookingsController@store');
                      Route::get('{event_booking}')->name('show')->uses('EventBookingsController@show');
-                     Route::get('{event_booking}/edit')->name('edit')->uses('EventBookingsController@form');
+                     Route::get('{event_booking}/edit')->name('edit')->uses('EventBookingsController@edit');
                      Route::put('{event_booking}/edit')->name('update')->uses('EventBookingsController@update');
                      Route::delete('{event_booking}')->name('destroy')->uses('EventBookingsController@destroy');
                  });
