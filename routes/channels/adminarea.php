5f39a87e96dc60ba5e16752122cd9694b05b753c
Abdelrahman Omran <me@omranic.com>
2020-12-05 12:17:51 +0400

    Type hint Authorizable user parameter

--- routes/channels/adminarea.php
+++ routes/channels/adminarea.php
@@ -2,22 +2,24 @@
 
 declare(strict_types=1);
 
-Broadcast::channel('adminarea-services-index', function ($user) {
+use Illuminate\Contracts\Auth\Access\Authorizable;
+
+Broadcast::channel('adminarea-services-index', function (Authorizable $user) {
     return $user->can('list', app('cortex.bookings.service'));
 }, ['guards' => ['admin']]);
 
-Broadcast::channel('adminarea-services-bookings-index', function ($user) {
+Broadcast::channel('adminarea-services-bookings-index', function (Authorizable $user) {
     return $user->can('list', app('cortex.bookings.service.booking'));
 }, ['guards' => ['admin']]);
 
-Broadcast::channel('adminarea-events-index', function ($user) {
+Broadcast::channel('adminarea-events-index', function (Authorizable $user) {
     return $user->can('list', app('cortex.bookings.event'));
 }, ['guards' => ['admin']]);
 
-Broadcast::channel('adminarea-tickets-index', function ($user) {
+Broadcast::channel('adminarea-tickets-index', function (Authorizable $user) {
     return $user->can('list', app('cortex.bookings.ticket'));
 }, ['guards' => ['admin']]);
 
-Broadcast::channel('adminarea-tickets-bookings-index', function ($user) {
+Broadcast::channel('adminarea-tickets-bookings-index', function (Authorizable $user) {
     return $user->can('list', app('cortex.bookings.ticket.booking'));
 }, ['guards' => ['admin']]);
