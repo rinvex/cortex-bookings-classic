58c7ab4bd18ce87666472561dc57aebddc29bcfd
Abdelrahman Omran <me@omranic.com>
2020-12-10 03:43:45 +0400

    Move custom eloquent model events to module layer from core package layer

--- routes/channels/channels.php
+++ routes/channels/channels.php
@@ -4,22 +4,22 @@ declare(strict_types=1);
 
 use Illuminate\Contracts\Auth\Access\Authorizable;
 
-Broadcast::channel('rinvex.bookings.services.index', function (Authorizable $user) {
+Broadcast::channel('cortex.bookings.services.index', function (Authorizable $user) {
     return $user->can('list', app('cortex.bookings.service'));
 }, ['guards' => ['admin']]);
 
-Broadcast::channel('rinvex.bookings.services.bookings.index', function (Authorizable $user) {
+Broadcast::channel('cortex.bookings.services.bookings.index', function (Authorizable $user) {
     return $user->can('list', app('cortex.bookings.service.booking'));
 }, ['guards' => ['admin']]);
 
-Broadcast::channel('rinvex.bookings.events.index', function (Authorizable $user) {
+Broadcast::channel('cortex.bookings.events.index', function (Authorizable $user) {
     return $user->can('list', app('cortex.bookings.event'));
 }, ['guards' => ['admin']]);
 
-Broadcast::channel('rinvex.bookings.tickets.index', function (Authorizable $user) {
+Broadcast::channel('cortex.bookings.tickets.index', function (Authorizable $user) {
     return $user->can('list', app('cortex.bookings.ticket'));
 }, ['guards' => ['admin']]);
 
-Broadcast::channel('rinvex.bookings.tickets.bookings.index', function (Authorizable $user) {
+Broadcast::channel('cortex.bookings.tickets.bookings.index', function (Authorizable $user) {
     return $user->can('list', app('cortex.bookings.ticket.booking'));
 }, ['guards' => ['admin']]);
