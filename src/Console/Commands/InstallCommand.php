5fffb3729aa8f0e86486fd056ef4eec9606a37a2
Abdelrahman Omran <me@omranic.com>
2021-01-18 10:48:06 +0400

    Skip publishing module resources unless explicitly specified, for simplicity

--- src/Console/Commands/InstallCommand.php
+++ src/Console/Commands/InstallCommand.php
@@ -31,7 +31,8 @@ class InstallCommand extends Command
     {
         $this->alert($this->description);
 
-        $this->call('cortex:publish:bookings', ['--force' => $this->option('force'), '--resource' => $this->option('resource')]);
+        ! $this->option('resource') || $this->call('cortex:publish:bookings', ['--force' => $this->option('force'), '--resource' => $this->option('resource')]);
+
         $this->call('cortex:migrate:bookings', ['--force' => $this->option('force')]);
         $this->call('cortex:seed:bookings');
     }
